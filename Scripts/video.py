57622c20b2ab2ece486d67795060938d12f58c2a
gysong <gysong@mit.edu>
2022-01-20 00:46:37 -0500

    commented out length bug

--- Scripts/video.py
+++ Scripts/video.py
@@ -23,7 +23,7 @@ def get_frame_information(video_file_path):
     video_frames = [frame for frame in output['frames'] if frame['media_type'] == 'video']
     frame_times = [frame["pkt_pts_time"] for frame in video_frames]
     video_stream_info = next(s for s in output['streams'] if s['codec_type'] == 'video')
-    assert len(frame_times) == int(video_stream_info["nb_frames"])
+    # assert len(frame_times) == int(video_stream_info["nb_frames"])
 
     # convert to milliseconds
     frame_times_ms = [int(1000*float(x)) for x in frame_times]
